ba3c12bc94edaa6b38f5b9727e7d80e89170cec0
pasleto <tomas.pasler@gmail.com>
2019-08-30 18:09:48 +0200

    NodeJS host for PHP files

---
+++ nodejs-php-host.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var app = express();
+
+var phpExpress = require('php-express')({
+  binPath: 'php'
+});
+
+// set view engine to php-express
+app.set('views', './views');
+app.engine('php', phpExpress.engine);
+app.set('view engine', 'php');
+
+// routing all .php file to php-express
+app.all(/.+\.php$/, phpExpress.router);
+
+var server = app.listen(80, function () {
+      console.log(``);
+    console.log(`----------------------------------------------------------------------------------`);
+    console.log(`----------------------------------------------------------------------------------`);
+    console.log(``);
+    console.log(`Server running on http://localhost/`);
+    console.log(``);
+    console.log(`----------------------------------------------------------------------------------`);
+    console.log(`----------------------------------------------------------------------------------`);
+});
