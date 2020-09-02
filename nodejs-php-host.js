5ce8ea2d630a1534d35d0a830fa7416b5f1ebcdd
LukasBartipan <lukasbatipan@gmail.com>
2020-09-02 12:28:31 +0200

    added public folder

--- nodejs-php-host.js
+++ nodejs-php-host.js
@@ -2,16 +2,30 @@ var express = require('express');
 var app = express();
 var phpExpress = require('php-express')({binPath: 'php'});
 
+// ---------------- //
+   var PORT = 3000; 
+// ---------------- //
+
 // set view engine to php-express
 app.set('views', './views');
 app.engine('php', phpExpress.engine);
 app.set('view engine', 'php');
 
+// let express know there's a public directory.
+app.use(express.static('./public'));
+
 // routing all .php file to php-express
 app.all(/.+\.php$/, phpExpress.router);
 
-app.listen(80, function () {
+// render default index page
+app.get('/', function(req, res) {
+    // res.send is changed to result.render in order to load the correct view.
+    res.render('index');
+});
+
+// server start
+app.listen(PORT, function () {
     console.log(`--------------------------------------`);
-    console.log(`Server is running on http://localhost/`);
+    console.log(`Server is running on http://localhost:` + PORT + `/`);
     console.log(`--------------------------------------`);
 });
---
+++ views/index.php
@@ -0,0 +1,17 @@
+<html>
+	<head>
+		<title>Node.js PHP Hosting</title>
+		<link rel="stylesheet" href="/css/style.css" type="text/css">
+	</head>
+	<body>
+		<div id="mySidenav" class="sidenav">
+			<a href="#">Hello</a>
+		  	<a href="#">World</a>
+		</div>
+		<div class="main">
+			<h1>Hello,</h1>
+			<?php echo '<h2>World</h2>'?> 
+		</div>
+		<script src="/scripts/script.js"></script>
+	</body>
+</html>
